27adea0db009b8d8993aad58b44a4b69b62bf266
Jess Li <116328975+jessl916@users.noreply.github.com>
2026-01-09 12:51:32 -0600

    add examples in k_shot_prompting.py
    Updated system prompt with additional examples for reversing letters in words.

--- week1/k_shot_prompting.py
+++ week1/k_shot_prompting.py
@@ -8,11 +8,14 @@ NUM_RUNS_TIMES = 5
 
 # TODO: Fill this in!
 YOUR_SYSTEM_PROMPT = """ You reverse letters in words like the following examples: 
-<example> given word = "researchers", steps: r-e-s-e-a-r-c-h-e-r-s -> s-r-e-h-c-r-a-e-s-e-r, the reversed word is "srehcraeser" </example> 
+<example> input: "cat", steps: c-a-t -> t-a-c, output: "tac" </example>
+<example> given word = "python", steps: p-y-t-h-o-n -> n-o-h-t-y-p, the reversed word is "nohtyp" </example> 
+<example> input: "awesome", steps: a-w-e-s-o-m-e -> e-m-o-s-e-w-a, output: "emosewa" </example> 
+<example> given word = "researcher", steps: r-e-s-e-a-r-c-h-e-r -> r-e-h-c-r-a-e-s-e-r, the reversed word is "srehcraeser" </example> 
+<example> given word = "globalization", steps: g-l-o-b-a-l-i-z-a-t-i-o-n -> n-o-i-t-a-z-i-l-a-b-o-l-g , the reversed word is "noitazilabolg" </example> 
+
 """
-# <example> input: "cat", steps: c-a-t -> t-a-c, output: "tac" </example>
-# <example> given word = "python", steps: p-y-t-h-o-n -> n-o-h-t-y-p, the reversed word is "nohtyp" </example> 
-# <example> input: "awesome", steps: a-w-e-s-o-m-e -> e-m-o-s-e-w-a, output: "emosewa" </example> 
+
 
 
 USER_PROMPT = """
